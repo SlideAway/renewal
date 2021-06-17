321eb84caf686a22a08ae1135f0bc17b36860e8d
wonderwall <gkszm636@gmail.com>
2021-06-17 16:59:50 +0900

    버튼 종류 추가

---
+++ src/component/atom/button/FilledButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Button} from "antd";
+
+const FilledButton = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+FilledButton.propTypes = {
+
+};
+
+export default FilledButton;
