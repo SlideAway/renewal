8d9a486cfd5147fbf6d2dc7a4978155da457482c
gkszm <gkszm636@gmail.com>
2021-06-17 21:31:43 +0900

    패스워드에 툴팁 추가

--- src/component/atom/input/TPassword.js
+++ src/component/atom/input/TPassword.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {Input} from "antd";
+import {Input, Tooltip} from "antd";
 import {Controller} from "react-hook-form";
 
 const TPassword = (props) => {
+    const [capslock, setCapslock] = useState(false);
+    const [focus, setFocus] = useState(false);
+
+    window.addEventListener('keydown', (e) => setCapslock(e.getModifierState("CapsLock")))
+
     return (
         <Controller
             {...props}
+
             defaultValue=''
             render={(innerProps) => (
+                <Tooltip  placement="top" visible={focus && capslock} trigger='focus' title={capslock ?  'CapsLock이 켜져있습니다. ':'' }>
                 <Input.Password
                     {...props}
                     {...innerProps}
+                    onFocus={() => setFocus(true)}
+                    onBlur={(e) => {
+                        setFocus(false);
+                        innerProps.onBlur(e);
+                    }}
                 />
+                </Tooltip>
             )}
         />
     );
