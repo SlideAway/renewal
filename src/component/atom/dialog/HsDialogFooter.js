a10f6de1cbf51d28d11e4088ef8f8f08f94d1c39
wonderwall <gkszm636@gmail.com>
2021-06-23 13:42:30 +0900

    dialog footer에 button 끼리 Space 적용

--- src/component/atom/dialog/HsDialogFooter.js
+++ src/component/atom/dialog/HsDialogFooter.js
@@ -2,24 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Classes} from "@blueprintjs/core";
 import HsButton from "../button/HsButton";
+import {Space} from "antd";
 
-const HsDialogFooter = ({content, formState, setShow}) => {
+const HsDialogFooter = ({content, setShow}) => {
     return (
         <div className={Classes.DIALOG_FOOTER}>
-            <HsButton onClick={() => setShow(false)}>닫기</HsButton>
-            {content}
+            <Space>
+                {content}
+                <HsButton onClick={() => setShow(false)} type='danger'>닫기</HsButton>
+            </Space>
         </div>
     );
 };
 
 HsDialogFooter.defaultProps = {
-    content:<></>
+    content: <></>
 }
 
 HsDialogFooter.propTypes = {
-    content:PropTypes.element,
-    formState:PropTypes.object.isRequired,
-    setShow:PropTypes.func.isRequired
+    content: PropTypes.element,
+    formState: PropTypes.object.isRequired,
+    setShow: PropTypes.func.isRequired
 };
 
 export default HsDialogFooter;
