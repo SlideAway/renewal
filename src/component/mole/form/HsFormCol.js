b8ed99266f78fbe9e8f555840d89e5baa7e63d53
wonderwall <gkszm636@gmail.com>
2021-06-23 17:03:18 +0900

    FormCol에 기존 코드 적용

--- src/component/mole/form/HsFormCol.js
+++ src/component/mole/form/HsFormCol.js
@@ -3,11 +3,37 @@ import HsInputCollection from "./HsInputCollection";
 import HsErrorMessage from "../../atom/error/HsErrorMessage";
 import {Form} from "antd";
 import PropTypes from 'prop-types';
+import HsCol from "../../atom/layout/HsCol";
+
+export const defaultSize = {
+    xs: 12,
+    sm: 6,
+    md: 5,
+    lg: 4,
+    xl: 3
+}
+
+export const setGridCol = (item) => {
+    const size = [];
+    if (item.size) {
+        if (item.size.xs) size.push('ant-col-xs-' + item.size.xs)
+        if (item.size.sm) size.push('ant-col-sm-' + item.size.sm)
+        if (item.size.md) size.push('ant-col-md-' + item.size.md)
+        if (item.size.lg) size.push('ant-col-lg-' + item.size.lg)
+        if (item.size.xl) size.push('ant-col-xl-' + item.size.xl)
+
+    } else {
+        for (let key of Object.keys(defaultSize)) {
+            size.push(`col-${key}-${defaultSize[key]}`);
+        }
+    }
+    return size.join(' ');
+}
 
 const HsFormCol = ({item, control, errors}) => {
-
     return (
         <>
+            <HsCol {...defaultSize}>
             <Form layout='vertical' hidden={item.type === 'hidden'}>
                 <Form.Item label={item.name} required={item.required}>
                 <HsInputCollection item={item} control={control} rules={{
@@ -34,6 +60,7 @@ const HsFormCol = ({item, control, errors}) => {
                 {errors[item.id] ? <HsErrorMessage message={errors[item.id].message}/> : <></>}
                 </Form.Item>
             </Form>
+            </HsCol>
         </>
     );
 };
