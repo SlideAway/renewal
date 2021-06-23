00d545ac49c6328ed4050cd5d9cfb277a392dfc5
wonderwall <gkszm636@gmail.com>
2021-06-23 15:29:11 +0900

    비밀번호 기본 규칙 설정

--- src/component/atom/input/HsPassword.js
+++ src/component/atom/input/HsPassword.js
@@ -12,13 +12,19 @@ const HsPassword = (props) => {
     return (
         <Controller
             {...props}
-
+            rules={{
+                ...props.rules,
+                pattern:{
+                    value:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/,
+                    message:'비밀번호는 대, 소문자, 숫자, 특수문자를 포함하여 최소 8글자이어야 합니다. '
+                }}}
             defaultValue=''
             render={(innerProps) => (
                 <Tooltip  placement="top" visible={focus && capslock} trigger='focus' title={capslock ?  'CapsLock이 켜져있습니다. ':'' }>
                 <Input.Password
                     {...props}
                     {...innerProps}
+                    placeholder='대, 소문자, 숫자, 특수문자를 포함 8글자 이상. '
                     onFocus={() => setFocus(true)}
                     onBlur={(e) => {
                         setFocus(false);
@@ -32,7 +38,6 @@ const HsPassword = (props) => {
 };
 
 HsPassword.propTypes = {
-
 };
 
 export default HsPassword;
