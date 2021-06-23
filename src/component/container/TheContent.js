7c94ae58003e330abc0afac09ec73b3205db2003
wonderwall <gkszm636@gmail.com>
2021-06-23 17:04:33 +0900

    content에 padding 추가 및 PropTypes 제거

--- src/component/container/TheContent.js
+++ src/component/container/TheContent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {Route, Switch} from "react-router-dom";
 import routes from "../../routes";
 import {Layout} from "antd";
@@ -8,7 +7,7 @@ const {Content} = Layout
 
 const CustomContent = styled(Content)`
     margin:24px 16px 0;
-    //padding:24px;
+    padding:24px;
     min-height:360px;
 `
 
@@ -34,8 +33,5 @@ const TheContent = () => {
     );
 };
 
-TheContent.propTypes = {
-
-};
 
 export default TheContent;
