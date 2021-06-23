095b97e86827e9dc51fbf8aae8936b6c8042617a
wonderwall <gkszm636@gmail.com>
2021-06-23 17:03:31 +0900

    FormRow에 Row 추가

--- src/component/mole/form/HsFormRow.js
+++ src/component/mole/form/HsFormRow.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HsFormCol from "./HsFormCol";
+import HsRow from "../../atom/layout/HsRow";
 
-const HsFormRow = ({items, control, errors}) => {
+const HsFormRow = ({items, control, errors, rowProps}) => {
     return (
         <>
+            <HsRow {...rowProps}>
             {items.map(item => (
                 <HsFormCol item={item} control={control} errors={errors}/>
             ))}
+            </HsRow>
         </>
     );
 };
@@ -15,7 +18,8 @@ const HsFormRow = ({items, control, errors}) => {
 HsFormRow.propTypes = {
     items:PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
     control:PropTypes.object.isRequired,
-    errors:PropTypes.object.isRequired
+    errors:PropTypes.object.isRequired,
+    rowProps:PropTypes.object
 };
 
 export default HsFormRow;
