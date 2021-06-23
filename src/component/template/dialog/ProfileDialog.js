ec3c21e69fefd32916ebc82b022aac0bf4fcfc20
wonderwall <gkszm636@gmail.com>
2021-06-23 13:40:47 +0900

    프로필 폼을 배열로 처리 및 저장기능 추가

--- src/component/template/dialog/ProfileDialog.js
+++ src/component/template/dialog/ProfileDialog.js
@@ -1,53 +1,102 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import HsDialog from "../../atom/dialog/HsDialog";
 import {useForm} from "react-hook-form";
-import HsFormCol from "../../mole/form/HsFormCol";
+import HsButton from "../../atom/button/HsButton";
+import HeaderContext from "../../../utils/contexts/HeaderContext";
+import jwtDecode from "jwt-decode";
+import useAxios from "../../../utils/hooks/useAxios";
+import HsSpinner from "../../atom/progress/HsSpinner";
+import HsFormRow from "../../mole/form/HsFormRow";
+import {useAlert} from "../../../utils/hooks/useAlert";
 
 const ProfileForm = ({control, errors}) => {
     return (
-    <>
-        <HsFormCol
-            control={control}
-            errors={errors}
-            item={{id: 'id', name: '사용자 ID', disabled: true}}
-        />
-        <HsFormCol
-            control={control}
-            errors={errors}
-            item={{id: 'name', name: '사용자 이름', required: true}}
-        />
-        <HsFormCol
-            control={control}
-            errors={errors}
-            item={{id: 'phone', name: '연락처'}}
-        />
-        <HsFormCol
-            control={control}
-            errors={errors}
-            item={{id: 'email', name: '이메일'}}
-        />
-    </>
+        <>
+            <HsFormRow errors={errors}
+                       control={control}
+                       items={[
+                           {id: 'userId', name: '사용자 ID', disabled: true},
+                           {id: 'userNm', name: '사용자 이름', required: true},
+                           {id: 'userTel', name: '연락처'},
+                           {id: 'userEmail', name: '이메일'},
+                           {id: 'roleCd', type: 'hidden'},
+                           {id: 'userSeq', type: 'hidden'},
+                           {id: 'ipChk', type: 'hidden'},
+                           {id: 'ipAddr', type: 'hidden'},
+                           {id: 'userDesc', type: 'hidden'},
+                       ]}/>
+        </>
     )
 }
 
 const ProfileDialog = ({show, setShow}) => {
-    const {control, handleSubmit, errors, formState} = useForm();
+    const {control, handleSubmit, errors, formState, reset} = useForm();
+    const {submit, loading} = useAxios()
+    const {showAlert} = useAlert();
+    const {state} = useContext(HeaderContext);
+    const {token} = state;
+    const userInfo = jwtDecode(token);
+
+    const doConfirm = (data) => {
+        showAlert(
+            'info',
+            '저장하시겠습니까?',
+            () => doSave(data)
+        );
+    }
+
+
+    const doSave = (data) => {
+        const config = {
+            url: `/users/${userInfo.aud}`,
+            method: 'put',
+            data: data
+        }
+        submit(config, (res) => {
+            reset(res && res.data);
+            setShow(false);
+        })
+    }
+
+    useEffect(() => {
+        if (show) {
+            const config = {
+                url: `/users/${userInfo && userInfo.aud}`,
+                method: 'get'
+            }
+            submit(config, (res) => {
+                reset(res && res.data.data);
+            })
+
+        }
+    }, [show])
+
 
     return (
         <HsDialog
             title='프로필 수정'
             show={show}
-            bodyContent={<ProfileForm control={control} errors={errors}/>}
+            bodyContent={
+                <HsSpinner loading={loading}>
+                    <ProfileForm control={control} errors={errors}/>
+                </HsSpinner>}
+            footerContent={
+                <HsButton
+                    onClick={handleSubmit(doConfirm)}
+                    disabled={!formState.isDirty}
+                    loading={loading}
+                >
+                    저장
+                </HsButton>}
             setShow={setShow}
-            formState={formState}
         />
     );
 };
 
 ProfileDialog.propTypes = {
-    show:PropTypes.bool.isRequired,
-    setShow:PropTypes.func.isRequired
+    show: PropTypes.bool.isRequired,
+    setShow: PropTypes.func.isRequired
 };
 
 export default ProfileDialog;
