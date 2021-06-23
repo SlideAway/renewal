3f6dd645496225e8770c7e0fec60923a6bd7a3d6
wonderwall <gkszm636@gmail.com>
2021-06-23 17:04:04 +0900

    SearchForm 일부 작성

--- src/component/template/search/SearchForm.js
+++ src/component/template/search/SearchForm.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HsFormRow from "../../mole/form/HsFormRow";
+import {useForm} from "react-hook-form";
 
 const SearchForm = props => {
     const {form} = props;
+    const {errors, control, formState} = useForm();
     return (
-        <div>
-
-        </div>
+        <>
+            <HsFormRow rowProps={{gutter:'24'}} errors={errors} items={form} control={control}/>
+        </>
     );
 };
 
 SearchForm.propTypes = {
+    form:PropTypes.arrayOf(PropTypes.shape({
 
+    }))
 };
 
 export default SearchForm;
