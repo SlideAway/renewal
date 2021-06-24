039c61529b39364e9212b51a21faf229bfbe06b8
wonderwall <gkszm636@gmail.com>
2021-06-24 10:36:36 +0900

    기능추가 및 코드개선

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,8 +1,9 @@
-import React, {useContext} from 'react';
+import React, {useEffect, useRef} from 'react';
 import ContextSearchForm from "../../component/template/search/ContextSearchForm";
 import ToastGrid from "../../component/atom/grid/ToastGrid";
 import ContentContext from "../../utils/contexts/ContentContext";
 import HsCard from "../../component/atom/card/HsCard";
+import HsButton from "../../component/atom/button/HsButton";
 
 const columns = [
     {name: 'userId', header: '사용자 ID'},
@@ -13,24 +14,41 @@ const columns = [
     {name: 'userDesc', header:'비고'}
 ]
 
+const searchFilter = [
+    {id: 'userId', name: '사용자 ID'},
+    {id: 'userNm', name: '사용자 이름'},
+    {
+        id: 'roleCd', name: '권한', type: 'select',
+        required: false,
+        url: '/combo/custom/roleInfo',
+    },
+]
+
+
+
 const Users = () => {
+    const gridApi = useRef(null);
+
+    useEffect(() => {
+        console.log(gridApi)
+    }, [gridApi])
+
+    const optionButtons = [
+        <HsButton type='text' block={true}>추가</HsButton>,
+    ]
 
     return (
         <>
             <HsCard>
                 <ContextSearchForm
                     url='/users'
+                    options={optionButtons}
                     context={ContentContext}
-                    form={[
-                        {id: 'userId', name: '사용자 ID'},
-                        {id: 'userNm', name: '사용자 이름'},
-                        {
-                            id: 'roleCd', name: '권한', type: 'select',
-                            required: false,
-                            url: '/combo/custom/roleInfo',
-                        },
-                    ]}/>
-                <ToastGrid context={ContentContext} columns={columns}/>
+                    form={searchFilter}/>
+                <ToastGrid
+                    context={ContentContext}
+                    gridController={gridApi}
+                    columns={columns}/>
             </HsCard>
         </>
 
