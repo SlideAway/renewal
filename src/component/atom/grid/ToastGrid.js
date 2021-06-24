a0aff5ac1be270b5e788d455da6a32cfa6bf998f
wonderwall <gkszm636@gmail.com>
2021-06-24 10:36:16 +0900

    이벤트 및 spinner, api 추가

--- src/component/atom/grid/ToastGrid.js
+++ src/component/atom/grid/ToastGrid.js
@@ -1,34 +1,52 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import Grid from "@toast-ui/react-grid";
+import HsSpinner from "../progress/HsSpinner";
 
 const ToastGrid = (props) => {
-    const {context, columns, data, gridController} = props;
+    const {context, columns, gridController, onClick} = props;
     const {state} = useContext(context);
-    const {gridData} = state;
-    
+    const {gridData, loading} = state;
+    const gridApi = useRef(null);
+
+    useEffect(() => {
+        if(gridController) gridController.current = gridApi.current;
+        }, [gridApi])
+
     return (
         <>
+            <HsSpinner loading={loading}>
             <Grid
                 columns={columns}
                 data={gridData && gridData.content}
-                rowHeight={25}
-                minRowHeight={25}
-                bodyHeight={200}
+                rowHeight={30}
+                minRowHeight={30}
+                bodyHeight={315}
                 virtualScrolling={true}
                 heightResizable={true}
-                rowHeaders={['rowNum']}
+                rowHeaders={['rowNum', 'checkbox']}
+                ref={gridApi}
+                onClick={() => {if(onClick) onClick()}}
             />
+            </HsSpinner>
         </>
     );
 };
+
+ToastGrid.defaultProps = {
+    gridController:null,
+    onClick:() => {}
+}
 
 ToastGrid.propTypes = {
     context:PropTypes.object.isRequired,
     columns:PropTypes.arrayOf(PropTypes.shape({
         name:PropTypes.string,
-        header:PropTypes.string
-    }))
+        header:PropTypes.string,
+        type:PropTypes.string,
+    })).isRequired,
+    gridController:PropTypes.object,
+    onClick:PropTypes.func
 };
 
 export default ToastGrid;
