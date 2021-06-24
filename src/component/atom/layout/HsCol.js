0f5de5a68cf004dfb9cf209d213ed8c466583b24
wonderwall <gkszm636@gmail.com>
2021-06-24 10:36:03 +0900

    col에 padding 추가

--- src/component/atom/layout/HsCol.js
+++ src/component/atom/layout/HsCol.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Col} from "antd";
+import styled from "styled-components";
 
+const CustomCol = styled(Col)`
+  padding-right: 5px;
+  padding-left: 5px;
+`
 
 const HsCol = (props) => {
     return (
-        <Col {...props}>
+        <CustomCol {...props}>
             {props.children}
-        </Col>
+        </CustomCol>
     );
 };
 
